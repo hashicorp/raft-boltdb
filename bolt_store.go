7662e1d3bf13d84fc6173f692a9651b219aa3191
Ryan Uber <ru@ryanuber.com>
2015-01-29 15:02:17 -0800

    Initialize buckets

--- bolt_store.go
+++ bolt_store.go
@@ -4,6 +4,16 @@ import (
 	"github.com/boltdb/bolt"
 )
 
+const (
+	// Permissions to use on the db file. This is only used if the
+	// database file does not exist and needs to be created.
+	dbFileMode = 0600
+
+	// Bucket names we perform transactions in
+	dbLogs = "logs"
+	dbConf = "conf"
+)
+
 type BoltStore struct {
 	// conn is the underlying handle to the db.
 	conn *bolt.DB
@@ -15,7 +25,7 @@ type BoltStore struct {
 // NewBoltStore takes a file path and returns a connected Raft backend.
 func NewBoltStore(path string) (*BoltStore, error) {
 	// Try to connect
-	handle, err := bolt.Open(path, 0600, nil)
+	handle, err := bolt.Open(path, dbFileMode, nil)
 	if err != nil {
 		return nil, err
 	}
@@ -25,5 +35,32 @@ func NewBoltStore(path string) (*BoltStore, error) {
 		conn: handle,
 		path: path,
 	}
+
+	// Set up our buckets
+	if err := store.initialize(); err != nil {
+		store.Close()
+		return nil, err
+	}
+
 	return store, nil
+}
+
+// initialize is used to set up all of the buckets.
+func (b *BoltStore) initialize() error {
+	// Create all the buckets
+	err := b.conn.Update(func(tx *bolt.Tx) error {
+		if _, err := tx.CreateBucketIfNotExists([]byte(dbLogs)); err != nil {
+			return err
+		}
+		if _, err := tx.CreateBucketIfNotExists([]byte(dbConf)); err != nil {
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+// Close is used to gracefully close the DB connection.
+func (b *BoltStore) Close() error {
+	return b.conn.Close()
 }
--- bolt_store_test.go
+++ bolt_store_test.go
@@ -4,6 +4,8 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+
+	"github.com/boltdb/bolt"
 )
 
 func TestNewBoltStore(t *testing.T) {
@@ -26,5 +28,26 @@ func TestNewBoltStore(t *testing.T) {
 	}
 	if _, err := os.Stat(fh.Name()); err != nil {
 		t.Fatalf("err: %s", err)
+	}
+
+	// Close the store so we can open again
+	if err := store.Close(); err != nil {
+		t.Fatalf("err: %s", err)
+	}
+
+	// Ensure our tables were created
+	db, err := bolt.Open(fh.Name(), dbFileMode, nil)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	tx, err := db.Begin(true)
+	if err != nil {
+		t.Fatalf("err: %s", err)
+	}
+	if _, err := tx.CreateBucket([]byte(dbLogs)); err != bolt.ErrBucketExists {
+		t.Fatalf("bad: %v", err)
+	}
+	if _, err := tx.CreateBucket([]byte(dbConf)); err != bolt.ErrBucketExists {
+		t.Fatalf("bad: %v", err)
 	}
 }
