be524b4428f6d16bd1fa3f4e306249e4c7cf6f65
Philip O'Toole <philip.otoole@yahoo.com>
2023-05-23 06:34:36 -0400

    Remove Migrate to v2 code

--- v2/bolt_store.go
+++ v2/bolt_store.go
@@ -2,12 +2,9 @@ package raftboltdb
 
 import (
 	"errors"
-	"fmt"
-	"os"
 	"time"
 
 	metrics "github.com/armon/go-metrics"
-	v1 "github.com/boltdb/bolt"
 	"github.com/hashicorp/raft"
 	"go.etcd.io/bbolt"
 )
@@ -298,66 +295,3 @@ func (b *BoltStore) Sync() error {
 	return b.conn.Sync()
 }
 
-// MigrateToV2 reads in the source file path of a BoltDB file
-// and outputs all the data migrated to a Bbolt destination file
-func MigrateToV2(source, destination string) (*BoltStore, error) {
-	_, err := os.Stat(destination)
-	if err == nil {
-		return nil, fmt.Errorf("file exists in destination %v", destination)
-	}
-
-	srcDb, err := v1.Open(source, dbFileMode, &v1.Options{
-		ReadOnly: true,
-		Timeout:  1 * time.Minute,
-	})
-	if err != nil {
-		return nil, fmt.Errorf("failed opening source database: %v", err)
-	}
-
-	//Start a connection to the source
-	srctx, err := srcDb.Begin(false)
-	if err != nil {
-		return nil, fmt.Errorf("failed connecting to source database: %v", err)
-	}
-	defer srctx.Rollback()
-
-	//Create the destination
-	destDb, err := New(Options{Path: destination})
-	if err != nil {
-		return nil, fmt.Errorf("failed creating destination database: %v", err)
-	}
-	//Start a connection to the new
-	desttx, err := destDb.conn.Begin(true)
-	if err != nil {
-		destDb.Close()
-		os.Remove(destination)
-		return nil, fmt.Errorf("failed connecting to destination database: %v", err)
-	}
-
-	defer desttx.Rollback()
-
-	//Loop over both old buckets and set them in the new
-	buckets := [][]byte{dbConf, dbLogs}
-	for _, b := range buckets {
-		srcB := srctx.Bucket(b)
-		destB := desttx.Bucket(b)
-		err = srcB.ForEach(func(k, v []byte) error {
-			return destB.Put(k, v)
-		})
-		if err != nil {
-			destDb.Close()
-			os.Remove(destination)
-			return nil, fmt.Errorf("failed to copy %v bucket: %v", string(b), err)
-		}
-	}
-
-	//If the commit fails, clean up
-	if err := desttx.Commit(); err != nil {
-		destDb.Close()
-		os.Remove(destination)
-		return nil, fmt.Errorf("failed commiting data to destination: %v", err)
-	}
-
-	return destDb, nil
-
-}
--- v2/bolt_store_test.go
+++ v2/bolt_store_test.go
@@ -4,13 +4,11 @@ import (
 	"bytes"
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"reflect"
 	"testing"
 	"time"
 
 	"github.com/hashicorp/raft"
-	v1 "github.com/hashicorp/raft-boltdb"
 	"go.etcd.io/bbolt"
 )
 
@@ -417,57 +415,3 @@ func TestBoltStore_SetUint64_GetUint64(t *testing.T) {
 	}
 }
 
-func TestBoltStore_MigrateToV2(t *testing.T) {
-
-	dir, err := ioutil.TempDir("", t.Name())
-	if err != nil {
-		t.Fatalf("err: %s", err)
-	}
-	defer os.RemoveAll(dir)
-
-	srcFile := filepath.Join(dir, "/sourcepath")
-	destFile := filepath.Join(dir, "/destpath")
-
-	// Successfully creates and returns a store
-	srcDb, err := v1.NewBoltStore(srcFile)
-	if err != nil {
-		t.Fatalf("failed creating source database: %s", err)
-	}
-	defer srcDb.Close()
-
-	// Set a mock raft log
-	logs := []*raft.Log{
-		testRaftLog(1, "log1"),
-		testRaftLog(2, "log2"),
-		testRaftLog(3, "log3"),
-	}
-
-	//Store logs source
-	if err := srcDb.StoreLogs(logs); err != nil {
-		t.Fatalf("failed storing logs in source database: %s", err)
-	}
-	srcResult := new(raft.Log)
-	if err := srcDb.GetLog(2, srcResult); err != nil {
-		t.Fatalf("failed getting log from source database: %s", err)
-	}
-
-	if err := srcDb.Close(); err != nil {
-		t.Fatalf("failed closing source database: %s", err)
-	}
-
-	destDb, err := MigrateToV2(srcFile, destFile)
-	if err != nil {
-		t.Fatalf("did not migrate successfully, err %v", err)
-	}
-	defer destDb.Close()
-
-	destResult := new(raft.Log)
-	if err := destDb.GetLog(2, destResult); err != nil {
-		t.Fatalf("failed getting log from destination database: %s", err)
-	}
-
-	if !reflect.DeepEqual(srcResult, destResult) {
-		t.Errorf("BoltDB log did not equal Bbolt log, Boltdb %v, Bbolt: %v", srcResult, destResult)
-	}
-
-}
